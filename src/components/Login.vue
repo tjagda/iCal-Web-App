15ece014d103757ac7a20f81f21f63d8d5e13cfc
Josh <agda0627@hotmail.com>
2020-03-20 23:40:00 -0400

    Started working on login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,41 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>Login</h1>
-    <p v-if="msg"> {{ msg }} </p>
-    <p>
-      Enter login information:
-    </p>
-    <input v-model="msg" placeholder="Username" />
-    <input placeholder="Password" />
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <h1>Login</h1>
+      </div>
+    </div>
+    <b-form @submit="onSubmit" v-if="show">
+      <div class="row justify-content-center">
+        <div class="col-4 text-left">
+          <label for="email">Email Address</label>
+          <b-form-input
+            id="email"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Your email"
+          ></b-form-input>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4 text-left">
+          <label for="password">Password</label>
+          <b-form-input
+            id="password"
+            v-model="form.password"
+            type="password"
+            required
+            placeholder="Your password"
+          ></b-form-input>
+        </div>
+      </div>
+      <b-button id="loginButton" type="submit" variant="primary">Login</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
-  props: {
-    msg: {
-      type: String,
-      default: null
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      show: true
     }
   },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+label, button {
+  margin-top: 20px;
 }
 </style>

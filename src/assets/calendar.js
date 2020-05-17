c9e786f97a04f4a22212c9139beff49336d78925
Josh <agda0627@hotmail.com>
2020-05-17 14:35:03 -0400

    Creating calendar UI template

--- src/assets/calendar.js
+++ src/assets/calendar.js
@@ -1,12 +1,27 @@
 /**
  * Summary. Calendar js module
- * Description. Collection of functions for frontend
+ * Description. Collection of variables + functions for frontend
  * 
  * @author Josh Agda
  * @version 0.1
  */
 
 import ICAL from 'ical.js';
+
+export const monthArr = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
 
 export function getAllComponents(iCalStr) {
     let jCal = ICAL.parse(iCalStr);
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,9 +1,63 @@
 <template>
     <transition>
         <b-container>
-            <div v-for="item in jCalEvents" :key="item.id">
+            <!-- <div v-for="item in jCalEvents" :key="item.id">
                 {{ item }}
-            </div>
+            </div> -->
+            <!-- {{ currentDate }} -->
+            <b-row>
+                <b-col class=calendarCol>
+                    {{ monthView }}
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col class=calendarCol>Sunday</b-col>
+                <b-col class=calendarCol>Monday</b-col>
+                <b-col class=calendarCol>Tuesday</b-col>
+                <b-col class=calendarCol>Thursday</b-col>
+                <b-col class=calendarCol>Friday</b-col>
+                <b-col class=calendarCol>Saturday</b-col>
+            </b-row>
+            <b-row>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+            </b-row>
+            <b-row>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+            </b-row>
+            <b-row>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+            </b-row>
+            <b-row>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+            </b-row>
+            <b-row>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+                <b-col class=calendarCol></b-col>
+            </b-row>
         </b-container>
     </transition>
 </template>
@@ -20,17 +74,30 @@ export default {
         return {
             jCal: null,
             jCalEvents: null,
+            currentDate: null,
+            dayView: null,
+            monthView: null
         }
     },
     mounted() {
         this.jCalEvents = iCal.getAllComponents(this.iCalStr);
+        this.currentDate = this.getCurrentDate();
+        this.dayView = this.currentDate.getDay();
+        this.monthView = iCal.monthArr[this.currentDate.getMonth()];
     },
     methods: {
-
+        getCurrentDate() {
+            let today = new Date();
+            return today;
+        }
     }
 }
 </script>
 
 <style scoped>
+.calendarCol {
+    border-style: solid;
+    min-height: 50px;
+}
 
 </style>
